d5e5afaa2d9fbb15bd57e20a5e3e16268758abb8
YoshiakiTatsumi <octopusgarden0617@gmail.com>
2016-10-08 10:50:01 +0000

    create message model

--- db/migrate/20161008102331_create_messages.rb
+++ db/migrate/20161008102331_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
       t.string :name
-      t.stirng :body
+      t.string :body
 
       t.timestamps null: false
     end
